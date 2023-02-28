845a73ecb3dffaeef1026ecb710f9d1acce7e48d
Student <venus.kabera@student.moringaschool.com>
2023-02-28 13:47:25 +0300

    done

--- app/models/baked_good.rb
+++ app/models/baked_good.rb
@@ -1,3 +1,11 @@
 class BakedGood < ActiveRecord::Base
+  belongs_to :bakery
   # add association macro here
+  def self.by_price
+    self.all.order(price: :desc)
+  end
+
+  def self.most_expensive
+    self.all.order(price: :desc).first
+  end
 end
